f3c28e4cb733a29236bd844b2ba671d7789ab992
yersultan2025 <ersultan005@internet.ru>
2025-02-11 22:15:43 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,13 @@
-git add bot.py
-git commit -m "Бот файлын қостым"
-git push origin main
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+
+TOKEN = "СЕНІҢ_БОТ_ТОКЕНІҢ"
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=["start"])
+async def start(message: types.Message):
+    await message.reply("Сәлем! Мен жұмыс істеп тұрмын!")
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
